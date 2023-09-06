1a09c2b5ae98504822dcf99883139d9647e1a0ed
saheedatt <afolabisaheedat1@gmail.com>
2023-09-06 09:50:43 +0100

    Data Fetched

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const apiKey = 'effd046944mshab918ceb096ff70p104d4cjsnd580b285ad6c';
-const apiUrl = 'https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4';
 const app = {
   pages: [],
   init: function () {
@@ -16,7 +14,7 @@ const app = {
 
     const currentPage = location.hash.replace('#', '');
     if (currentPage === 'explore') {
-      fetchAndDisplayProperties();
+      //displayProperties(properties)
     }
   },
   nav: function (ev) {
@@ -50,96 +48,51 @@ const app = {
     }
   },
 };
-document.addEventListener('DOMContentLoaded', app.init);
-
-/*const apiKey = 'effd046944mshab918ceb096ff70p104d4cjsnd580b285ad6c';
-const apiUrl = 'https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4';
-
-
-// Function to fetch property listings from the API
-async function fetchPropertyListings() {
-  try {
-    const response = await fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': apiKey,
-        'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-      },
-    });
-
-    if (!response.ok) {
-      console.error('Error fetching property listings. Status code:', response.status);
-      throw new Error('Failed to fetch property listings.');
+async function fetchData() {
+  const url = 'https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'effd046944mshab918ceb096ff70p104d4cjsnd580b285ad6c',
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
     }
-
-    const data = await response.json();
-    console.log('Response Data:', data);
-    return data;
-  } catch (error) {
-    console.error('Error fetching property listings:', error);
-    throw error;
+  };
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    displayProperties(result.hits);
+  }catch (error) {
+    console.error(error);
   }
 }
+fetchData();
 
-// Function to display property cards
-function displayPropertyCards(propertyListings) {
-  const cardContainer = document.getElementById('card-container');
-  const cardsPerRow = 3; // Number of cards per row
+function displayProperties(properties) {
+  let propertiesHtml = '';
 
-  for (let i = 0; i < propertyListings.length; i++) {
-    const property = propertyListings[i];
+  for (const property in properties) {
+    if (properties.hasOwnProperty(property)) {
+      const imageUrl = properties[property].coverPhoto ? properties[property].coverPhoto.url : '';
 
-    // Create a new card div
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    // Create and set the image element
-    const image = document.createElement('img');
-    image.src = property.imageUrl; // Replace with the property image URL
-    image.alt = property.title;
-
-    // Create and set the title element
-    const title = document.createElement('h2');
-    title.textContent = property.title;
-
-    // Create and set the price element
-    const price = document.createElement('p');
-    price.textContent = `Price: ${property.price}`;
-
-    // Create and set the location element
-    const location = document.createElement('p');
-    location.textContent = `Location: ${property.location}`;
-
-    // Append image, title, price, and location to the card
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(price);
-    card.appendChild(location);
-
-    // Append the card to the card container
-    cardContainer.appendChild(card);
-
-    // Check if we need to start a new row
-    if ((i + 1) % cardsPerRow === 0) {
-      // Add a row break div
-      const rowBreak = document.createElement('div');
-      rowBreak.classList.add('row-break');
-      cardContainer.appendChild(rowBreak);
+      propertiesHtml += `
+        <div class="property">
+        <img src="${imageUrl}" alt="${properties[property].title}" width="300" height="200">
+          <h3>${properties[property].title}</h3>
+          <p>ID: ${properties[property].id}</p>
+          <p>Price: ${properties[property].price}</p>
+          <p>Rooms: ${properties[property].rooms}</p>
+          <p>Baths: ${properties[property].baths}</p>
+          <p>Location: ${properties[property].area}</p>
+        </div>
+      `;
     }
   }
+
+  const explorePage = document.getElementById('explore');
+  explorePage.innerHTML = propertiesHtml;
 }
-
-// Main function to fetch and display property listings
-async function fetchAndDisplayProperties() {
-  try {
-    const propertyListings = await fetchPropertyListings();
-
-    // Display property cards
-    displayPropertyCards(propertyListings);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-document.addEventListener('DOMContentLoaded', app.init);
-*/
+document.addEventListener('DOMContentLoaded', () => {
+  app.init();
+  fetchData();
+});

eb0f560b9a9d03653fb0421893fbc3267027f5ef
Ayobami Ajayi <lynxdev32@gmail.com>
2023-09-07 11:43:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-async function fetchProperties() {
-  const url =
-    'https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4';
+const url ='https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4';
 
+const propertyDOM = document.querySelector('.property-center');
+
+const fetchData = async () => {
+  propertyDOM.innerHTML ='<div class="loading"></div>';
+ 
   const options = {
     method: 'GET',
     headers: {
@@ -9,7 +12,6 @@ async function fetchProperties() {
       'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
     },
   };
-
   try {
     const response = await fetch(url, options);
     const result = await response.json();
@@ -19,34 +21,32 @@ async function fetchProperties() {
     return [];
   }
 }
-function displayProperties(properties) {
-  const propertiesHtml = properties.map((property) => {
-    const imageUrl = property.coverPhoto ? property.coverPhoto.url : '';
-    return `
-      <div class="property">
-        <img src="${imageUrl}" alt="${property.title}" width="300" height="200">
-        <h3>${property.title}</h3>
-        <p>ID: ${property.id}</p>
-        <p>Price: ${property.price}</p>
-        <p>Rooms: ${property.rooms}</p>
-        <p>Baths: ${property.baths}</p>
-        <p>Location: ${property.area}</p>
-      </div>
-    `;
-  });
-
-  const explorePage = document.getElementById('explore');
-  explorePage.innerHTML = propertiesHtml.join('');
-}
-
-function init() {
-  fetchProperties()
-    .then((properties) => {
-      displayProperties(properties);
+const displayProperties = (properties) => {
+  const propertyList = properties
+    .map((property) => {
+      const imageUrl = property.coverPhoto ? property.coverPhoto.url : '';
+      return `
+        <div class="property">
+        <a class="single-property" href="houses.html?id=123&name=jane&age=25">
+          <img src="${imageUrl}" alt="${property.title}" width="300" class= "image" height="200">
+          <h3>${property.title}</h3>
+          <p>ID: ${property.id}</p>
+          <p class="price">Price: ${property.price}</p>
+          <p class="price">Rooms: ${property.rooms}</p>
+          <p class="price">Baths: ${property.baths}</p>
+          <p class="price">Location: ${property.area}</p>
+          </a>
+        </div>
+      `;
     })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+    .join('');
+    propertyDOM.innerHTML =   ` <div class="property-container">
+    ${propertyList}
+    </div>`
+};
+const start = async () => {
+  const data = await fetchData();
+  displayProperties(data);
+};
 
-document.addEventListener('DOMContentLoaded', init);
+start();

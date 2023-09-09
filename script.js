855968f39f8f90cdc0b55b134c7b7930a2ddd9f8
lynxdm <lynxdev32@gmail.com>
2023-09-09 23:05:26 +0000

    initial styling

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-async function fetchProperties() {
-  const url =
-    'https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4';
+const url =
+  "https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=5&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4";
+
+const propertyDOM = document.querySelector(".property-center");
+
+const fetchData = async () => {
+  propertyDOM.innerHTML = '<div class="loading"></div>';
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': 'effd046944mshab918ceb096ff70p104d4cjsnd580b285ad6c',
-      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+      "X-RapidAPI-Key": "effd046944mshab918ceb096ff70p104d4cjsnd580b285ad6c",
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   };
-
   try {
     const response = await fetch(url, options);
     const result = await response.json();
@@ -18,35 +21,52 @@ async function fetchProperties() {
     console.error(error);
     return [];
   }
-}
-function displayProperties(properties) {
-  const propertiesHtml = properties.map((property) => {
-    const imageUrl = property.coverPhoto ? property.coverPhoto.url : '';
-    return `
-      <div class="property">
-        <img src="${imageUrl}" alt="${property.title}" width="300" height="200">
-        <h3>${property.title}</h3>
-        <p>ID: ${property.id}</p>
-        <p>Price: ${property.price}</p>
-        <p>Rooms: ${property.rooms}</p>
-        <p>Baths: ${property.baths}</p>
-        <p>Location: ${property.area}</p>
-      </div>
-    `;
-  });
-
-  const explorePage = document.getElementById('explore');
-  explorePage.innerHTML = propertiesHtml.join('');
-}
-
-function init() {
-  fetchProperties()
-    .then((properties) => {
-      displayProperties(properties);
+};
+const displayProperties = (properties) => {
+  const propertyList = properties
+    .map((property) => {
+      const imageUrl = property.coverPhoto ? property.coverPhoto.url : "";
+      return `
+        <div class="property" id=${property.id}>
+          <img src="${imageUrl}" alt="${property.title}" class="property-image">
+          <h3>${property.title}</h3>
+          <div class="details">
+          <h2>${property.title}</h2>
+          <p class="price">Price: <span>${property.price}</span></p>
+          <p class="rooms">Rooms: <span>${property.rooms}</span></p>
+          <p class="baths">Baths: <span>${property.baths}</span></p>
+          <p class="location">Location: <span>${property.area}</span></p>
+          <p class="description">Description: <span>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repudiandae commodi, omnis optio sapiente mollitia dolores, magni minima ratione, qui quisquam. Exercitationem quam, nisi nemo voluptates voluptate in aut culpa enim mollitia dolor sequi ducimus excepturi maxime ipsam sapiente porro fugiat, quis necessitatibus? Commodi temporibus quia voluptatibus ad repudiandae nihil ut in. Culpa fugit nesciunt enim dolor cum voluptate veniam ipsum accusantium tempora! Perspiciatis voluptatibus cupiditate ullam, accusantium atque ad."</span></p>
+          </div>
+        </div>`;
     })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+    .join("");
+  propertyDOM.innerHTML = `<div class="property-container">${propertyList}</div>`;
+};
+const start = async () => {
+  const data = await fetchData();
+  console.log(data);
+  displayProperties(data);
+};
 
-document.addEventListener('DOMContentLoaded', init);
+start();
+
+// SIDEBAR FUNCTIONALITY
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-sidebar");
+const sidebar = document.querySelector("aside");
+const listElements = document.querySelectorAll("aside li");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.add("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
+
+listElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    sidebar.classList.remove("show-sidebar");
+  });
+});
